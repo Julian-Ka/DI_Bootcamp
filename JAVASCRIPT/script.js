d04ce01166526603bdaaf03f607622827fa8bcda
Julian Kassanke <julian.kassanke@gmail.com>
2021-04-19 22:04:56 +0300

    Portfolio

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,66 @@
+// //let variableName = value // variablendeklaration in js
+// //variablennamen immer erstes wort klein zweites wort mit grossem buchstaben anfangen
+// // Beispiel camelCase
+
+// let userName = "David";
+
+// //Declaration
+// let userName;
+
+// //Defining
+// userName = "David";
+
+
+// //VAR VAR wird nicht mehr benutzt
+// //LET
+// //CONST
+
+
+// //DATATYPES
+
+// //STRING everything in ""
+// "How are you?"
+// "Computer"
+
+// //number
+// 24
+
+
+// let weather = "Its sunny today";
+// let temperature = 24;
+// let city = "Tel Aviv";
+
+
+// //CONCATENATE mehrere variablen in einem satz z.B
+
+// // Its sunny today, its 24 degrees in Tel Aviv
+// let sentence = weather + "its" + temperature + "degrees in" + city;
+// let sentenceNew = `${weather} its ${temperature} degrees in ${city}`;
+
+
+// //built-in methods and properties
+
+// let userName = "David";
+// //methodenaufruf mit .methodenname("")
+// let userNameLength = userName.length
+
+
+// let addressNumber = 42;
+// console.log (addressNumber);
+// let addressStreet = "Teststreet";
+// console.log (addressStreet);
+// let country = "Israel";
+// console.log (country);
+
+// let global_address = `I live in ${addressStreet} ${addressNumber}, in ${country}`;
+// console.log(global_address)
+
+
+let birthyear = 1987;
+//console.log(birthyear);
+let futureyear = 2044;
+//console.log(futureyear);
+let age = futureyear - birthyear;
+//console.log(age);
+let sentence = `I will be ${age} in ${futureyear}`;
+console.log(sentence);
