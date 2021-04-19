87793b2d9731824e22ce630da84275a00e44f5ca
Julian Kassanke <julian.kassanke@gmail.com>
2021-04-19 23:07:59 +0300

    JS EX 1

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,66 +1,67 @@
-// //let variableName = value // variablendeklaration in js
-// //variablennamen immer erstes wort klein zweites wort mit grossem buchstaben anfangen
-// // Beispiel camelCase
-
-// let userName = "David";
-
-// //Declaration
-// let userName;
-
-// //Defining
-// userName = "David";
+let x = 5;
+let y = "34";
+let a = "4";
+let z = x + y;
+console.log(z);
+let b = x - a;
+console.log(b);
 
 
-// //VAR VAR wird nicht mehr benutzt
-// //LET
-// //CONST
+
+let c = 10;
+let d = 5;
+let e = c % d;
+console.log(e);
+
+let f = d % c;
+console.log(f);
 
 
-// //DATATYPES
-
-// //STRING everything in ""
-// "How are you?"
-// "Computer"
-
-// //number
-// 24
+let word1 = "Java";
+let word2 = "Script";
+let word3 = word1 + word2;
+console.log(word3);
 
 
-// let weather = "Its sunny today";
-// let temperature = 24;
-// let city = "Tel Aviv";
+let word4 = " ";
+let word5 = " ";
+let word = " ";
+let g = 0;
+let word6 = word4 + word5;
+console.log(word6);
+let word7 = word+ g;
+console.log(word7);
 
 
-// //CONCATENATE mehrere variablen in einem satz z.B
-
-// // Its sunny today, its 24 degrees in Tel Aviv
-// let sentence = weather + "its" + temperature + "degrees in" + city;
-// let sentenceNew = `${weather} its ${temperature} degrees in ${city}`;
-
-
-// //built-in methods and properties
-
-// let userName = "David";
-// //methodenaufruf mit .methodenname("")
-// let userNameLength = userName.length
+let b1 = true;
+let b2 = true;
+let c1 = b1 + b2;
+console.log(c1);
 
 
-// let addressNumber = 42;
-// console.log (addressNumber);
-// let addressStreet = "Teststreet";
-// console.log (addressStreet);
-// let country = "Israel";
-// console.log (country);
-
-// let global_address = `I live in ${addressStreet} ${addressNumber}, in ${country}`;
-// console.log(global_address)
+let b3 = true;
+let b4 = false;
+let c2 = b3 + b4;
+console.log(c2);
 
 
-let birthyear = 1987;
-//console.log(birthyear);
-let futureyear = 2044;
-//console.log(futureyear);
-let age = futureyear - birthyear;
-//console.log(age);
-let sentence = `I will be ${age} in ${futureyear}`;
-console.log(sentence);
+let b5 = false;
+let b6 = true;
+let c3 = b5 + b6;
+console.log(c3);
+
+let b7 = false;
+let b8 = true;
+let c4 = b7 - b8;
+console.log(c4);
+
+let num1 = 3;
+let num2 = 4;
+let num3 = num1 - num2;
+console.log(num3);
+
+let name1 = "Bob";
+let name2 = "bill";
+let name3 = name1 - name2;
+console.log(name3);
+    
