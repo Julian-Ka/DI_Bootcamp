d0c66ab52f73f4b51e203067acd90fa0a24b9a62
Julian Kassanke <julian.kassanke@gmail.com>
2021-05-13 12:38:48 +0300

    Daily Challenge

---
+++ JAVASCRIPT/Week6/Day4/DailyChallenge/script.js
@@ -0,0 +1,33 @@
+let groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        payed : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
+
+//let newGroc = groceries;
+//console.log(newGroc);
+
+ //groceries.totalPrice = "35$";
+//console.log(groceries);
+//console.log(newGroc);
+
+//groceries.other.payed = false;
+//console.log(groceries);
+//console.log(newGroc);
+
+// const groceriesJson = JSON.stringify(groceries);
+// let newGroc = JSON.parse(groceriesJson)
+//  groceries.totalPrice = "35$";
+//  //console.log(groceries);
+//  //console.log(newGroc);
+
+//   groceries.other.payed = false;
+//  console.log(groceries);
+//  console.log(newGroc);
+
+
+// json method makes a copy by value and the normal method just copies by reference
