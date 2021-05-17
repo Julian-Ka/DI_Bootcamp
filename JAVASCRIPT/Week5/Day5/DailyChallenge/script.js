a749ceb401ec0e8ce4ea123b61b52504be94595e
Julian Kassanke <julian.kassanke@gmail.com>
2021-05-17 11:26:53 +0300

    DailyChallenge

---
+++ JAVASCRIPT/Week5/Day5/DailyChallenge/script.js
@@ -0,0 +1,22 @@
+const writeA = () => {
+
+    document.getElementById("b3").innerHTML = "*"
+    document.getElementById("b2").innerHTML = "*"
+    document.getElementById("bc").innerHTML = "*"
+    document.getElementById("b1").innerHTML = "*"
+    document.getElementById("3a").innerHTML = "*"
+    document.getElementById("4a").innerHTML = "*"
+    document.getElementById("eb").innerHTML = "*"
+    document.getElementById("e1").innerHTML = "*"
+    document.getElementById("ec").innerHTML = "*"
+    document.getElementById("e2").innerHTML = "*"
+    document.getElementById("e3").innerHTML = "*"
+    document.getElementById("cc").innerHTML = "*"
+    document.getElementById("dc").innerHTML = "*"
+    document.getElementById("bb").innerHTML = "*"
+    
+
+    
+}
+
+writeA();
