1ef4c1d2df134b1e7e1d4db5f7a1aa9a4be1aa13
Julian Kassanke <julian.kassanke@gmail.com>
2021-05-28 20:52:38 +0300

    Exercise

---
+++ JAVASCRIPT/Week8/Day4/Exercises/script.js
@@ -0,0 +1,12 @@
+//Exercise 1
+
+// fetch('https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&limit=25&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My')
+// .then(response => response.json())
+// .then(data => console.log(data));
+
+
+//Exercise 2 retrieving 10 gifs about sun
+
+// fetch('https://api.giphy.com/v1/gifs/search?q=sun&rating=g&limit=10&offset=2&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My')
+// .then(response => response.json())
+// .then(data => console.log(data));
